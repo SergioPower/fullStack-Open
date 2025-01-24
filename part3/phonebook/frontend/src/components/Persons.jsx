12b3ef5c6f85afe0f13d72761e4704713468875b
Sergio Guzman <serguz95444@gmail.com>
2025-01-24 17:10:48 -0600

    Refactor componente Persons: actualizar estilos y mejorar la semántica de la lista de contactos

--- part3/phonebook/frontend/src/components/Persons.jsx
+++ part3/phonebook/frontend/src/components/Persons.jsx
@@ -1,18 +1,19 @@
-import styles from '../styles/Persons.module.css'
+import styles from '../styles/ContactList.module.css';
 
 const Persons = ({ personsToShow, deletePerson }) => {
-	
   return (
-    <ul className={styles.numbers}>
-      {personsToShow.map(person => 
-        <li key={person.id} className={styles.contact}>
-          <span>{person.name}</span>
-          <p>{person.number}</p>
-          <button onClick={() => deletePerson(person.id)}>Delete</button>
-          
+    <ul className={styles.contactList}>
+      {personsToShow.map(person => (
+        <li key={person.id} className={styles.contactItem}>
+          <span className={styles.contactName}>{person.name}</span>
+          <span className={styles.contactNumber}>{person.number}</span>
+          <button className={styles.deleteButton} onClick={() => deletePerson(person.id)}>
+            Delete
+          </button>
         </li>
-      )}
+      ))}
     </ul>
-  )}
+  );
+};
 
 export default Persons
