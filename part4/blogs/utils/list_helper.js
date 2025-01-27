d09f8aee5db3c45c8035ca1bf2c54ef9f3b25fc4
Sergio Guzman <serguz95444@gmail.com>
2025-01-27 15:25:39 -0600

    Agregar función para obtener el blog favorito y pruebas unitarias correspondientes

--- part4/blogs/utils/list_helper.js
+++ part4/blogs/utils/list_helper.js
@@ -5,8 +5,22 @@ const dummy = (blogs) => {
 const totalLikes = (blogs) => {
 	return blogs.reduce((sum, blog) => sum + blog.likes, 0);
 };
+
+const favoriteBlog = (blogs) => {
+	const mostLikedBlog = blogs.reduce((max, blog) => {
+	  return blog.likes > max.likes ? blog : max;
+	}, { likes: 0 });
+  
+	return {
+	  title: mostLikedBlog.title,
+	  author: mostLikedBlog.author,
+	  likes: mostLikedBlog.likes
+	};
+};
+  
   
 module.exports = {
 	dummy,
-	totalLikes
+	totalLikes,
+	favoriteBlog
 };
