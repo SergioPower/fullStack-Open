afb0acbb7fd768abacfdc4fb1af5e4dcd16ede43
Sergio Guzman <serguz95444@gmail.com>
2025-01-27 14:51:07 -0600

    Agregar pruebas unitarias para la lista de libros

--- part4/blogs/eslint.config.mjs
+++ part4/blogs/eslint.config.mjs
@@ -5,15 +5,16 @@ import pluginJs from "@eslint/js";
 export default [
     {
         files: ["**/*.js"], // Aplica a todos los archivos JavaScript
+        ignores: ["node_modules/**", "dist/**", "build/**"], // Ignora estos directorios
         languageOptions: {
             globals: globals.node, // Usa las variables globales de Node.js
         },
         rules: {
-            "indent": ["error", "tab"], // Usar tabulaciones en lugar de espacios
+            "indent": ["error", "tab"], // Usar tabulaciones para la indentación
             "no-tabs": "off", // Desactiva la regla que prohíbe tabulaciones
             "quotes": ["error", "single"], // Usar comillas simples
             "semi": ["error", "always"], // Poner punto y coma al final
-            "no-multiple-empty-lines": ["error", { max: 1 }], // Máximo 1 línea vacía al final
+            "no-mixed-spaces-and-tabs": "error", // Prohibir mezcla de espacios y tabulaciones
         },
     },
     pluginJs.configs.recommended, // Usa la configuración recomendada de ESLint
